1d1986f0278689b29afe67b942788685cf0c0102
LeraPetrovich <valeria.petrovich@northitgroup.eu>
2025-05-25 17:20:44 +0300

    create loaders and lomits

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,10 +1,12 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
-import { schema } from './schema.js';
+import { parse, validate, execute } from 'graphql';
+import { createSchema } from './schema.js';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
-  // const { prisma } = fastify;
+  const { prisma } = fastify;
+  const schema = createSchema(prisma);
 
   fastify.route({
     url: '/',
@@ -17,9 +19,14 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     },
     async handler(req) {
       const { query, variables } = req.body;
-      const result = await graphql({
+      const document = parse(query);
+      const errors = validate(schema, document, [depthLimit(5)]);
+      if (errors.length > 0) {
+        return { errors };
+      }
+      const result = await execute({
         schema,
-        source: query,
+        document,
         variableValues: variables,
         contextValue: {
           prisma: fastify.prisma,
